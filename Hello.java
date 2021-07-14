008adf2e784febd336e7e3febacafee3851b600d
sergiocpxfontes <79989206+sergiocpxfontes@users.noreply.github.com>
2021-07-14 19:58:00 +0100

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -2,5 +2,6 @@ public class Hello {
    public static void main(String[] args) {
       // Prints "Hello, World" in the terminal window.
       System.out.println("Isto é um exemplo para execução no Jenkins");
+      System.out.println("O argumento é" + args[0]);
    }
 }
