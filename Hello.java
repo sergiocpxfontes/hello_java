bf1d75c2f1b70fcba62d24ba0dcb9123435cfe43
sergiocpxfontes <79989206+sergiocpxfontes@users.noreply.github.com>
2021-07-28 16:34:05 +0100

    input através de argumentos

--- Hello.java
+++ Hello.java
@@ -4,6 +4,6 @@ public class Hello {
       System.out.println("mais uma linha");
       System.out.println("mais uma linha");
       System.out.println("mais uma linha");
-      //System.out.println("O argumento é" + args[0]);
+      System.out.println("O argumento é" + args[0]);
    }
 }
