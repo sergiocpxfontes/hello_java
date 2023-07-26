129ffa8356f71d50b8238abfe9a706d0c32ded61
Sérgio Fontes <79989206+sergiocpxfontes@users.noreply.github.com>
2023-07-26 20:46:53 +0100

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,6 +1,8 @@
 public class Hello {
    public static void main(String[] args) {
       System.out.println("Isto é um exemplo para execução no Jenkins");
+      /*
       System.out.println("O argumento: " + args[0]);
+      */
    }
 }
